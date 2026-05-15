826f148951a631e873c382f11972451369abe7ef
mon <yanmonstertraklck@gmail.com>
2026-05-15 22:37:30 +0600

    ffff

---
+++ ConsoleCpp/ConsoleCpp.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	
+	double a, b;
+	int c;
+	std::cout << "Калькулятор\n\n\n Первое значение: ";
+	std::cin >> a;
+	std::cout << "Второе значение: ";
+	std::cin >> b;
+	std::cout << "Ваше дейсвтие\n(+ - * /): ";
+	std::cin >> c;
+	
+	if (b == 0 && c == 4) {
+		std::cout << "Нельзя!";
+	}
+	switch (c) {
+	case 1:
+		std::cout << a + b;
+		break;
+	case 2:
+		std::cout << a - b;
+		break;
+	case 3:
+		std::cout << a * b;
+		break;
+	case 4:
+		std::cout << a / b;
+		break;
+	default:
+		std::cout << "Неверное действие!";
+	}
+	
+	return 0;
+}
